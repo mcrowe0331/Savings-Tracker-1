2b5269c87ecf8112e6895a90eed7776cc4bd44a7
Kem2523 <krystalemorris@gmail.com>
2023-02-21 19:29:38 -0600

    backend created

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(
+    process.env.ATLAS_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+      console.log("connected to mongo: ", process.env.ATLAS_URI);
+    }
+  );
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
