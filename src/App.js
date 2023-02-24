3c1dd7aa2a6d7b385fcd63cbe6e9a8890649a7be
Leonel Alanis <l.alanis1989@gmail.com>
2023-02-23 20:36:59 -0600

    Started frontend components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
 
+import Navbar from "./components/navbar.component"
+import SavingsList from "./components/savings-list.component"
+import EditSaving from "./components/edit-savings.component"
+import CreateSaving from "./components/create-saving.component"
+import CreateUser from "./components/create-user.component"
+
+ 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar />
+    <br/>
+    <Routes>
+      <Route path="/" exact element={<SavingsList/>} />
+      <Route path="/edit/:id" element={<EditSaving/>} />
+      <Route path="/create" element={<CreateSaving/>} />
+      <Route path="/user" element={<CreateUser/>} />
+    </Routes>
     </div>
+    </Router>
   );
 }
-
+ 
 export default App;
