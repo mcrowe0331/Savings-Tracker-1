604d09576aef68123aae3da1431d5988f526eadb
Leonel Alanis <l.alanis1989@gmail.com>
2023-02-26 10:09:43 -0600

    Added create user and edit savings stub components

---
+++ components/edit-savings.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EditSaving extends Component {
+    render () {
+        return (
+            <div>
+                <p>The EditSaving component</p>
+            </div>
+        )
+    }
+}
